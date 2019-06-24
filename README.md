4046fe414050e057676cad70bcd4f0fcd5cdd741
zijld <d.zijlstra1989@gmail.com>
2019-06-24 16:35:10 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Blog app NodeJs
+
+Learning node project
---
+++ index.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const bodyParser = require('body-parser');
+const keys = require('./config/keys');
+
+// Require all Models
+
+// Require passport service for authentication
+
+// Connects to MongoDB
+mongoose.Promise = global.Promise;
+mongoose.connect(keys.mongoURI, { useMongoClient: true });
+
+// Call express and create the app variable
+const app = express();
+
+// Parse all body's to json
+app.use(bodyParser.json());
+// Adds cookie days * hours * min * sec * milisec
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
+// Adds authentication to app
+// app.use(passport.initialize());
+// app.use(passport.session());
+
+// Require all routes
+
+// Checks if this app is running in production
+if (['production'].includes(process.env.NODE_ENV)) {
+  app.use(express.static('client/build'));
+
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve('client', 'build', 'index.html'));
+  });
+}
+
+// Decides if the app is running local on Port 5000
+// Or at takes the port from process.env.PORT
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Listening on port`, PORT);
+});
