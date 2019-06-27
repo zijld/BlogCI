7ec0d057a18e48d8e3c7ca9928beb3f7b6ab6b28
zijld <d.zijlstra1989@gmail.com>
2019-06-27 08:58:36 +0200

    Setup first Redis caching

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -34,7 +34,20 @@ module.exports = app => {
 
   // GET: All blogs by user.id
   app.get('/api/blogs', requireLogin, async (req, res) => {
-    const blog = await Blog.find({ _user: req.user.id });
-    res.send(blog);
+    const redis = require('redis');
+    const redisUrl = require('redis://127.0.0.1:6379');
+    const client = redis.createClient(redusUrl);
+
+    // Do we have any cached data in redis related
+    // to this query
+
+    // if yes, then respond to request right away
+    // and return
+
+    // if no, we need to respond to request
+    // and update our cache store the data
+
+    const blogs = await Blog.find({ _user: req.user.id });
+    res.send(blogs);
   });
 };
