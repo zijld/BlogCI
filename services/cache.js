192b2ed23a5c258fa01c9dd73742c0383a1096f8
zijld <d.zijlstra1989@gmail.com>
2019-07-01 06:20:34 +0200

    create caching key to queries

--- services/cache.js
+++ services/cache.js
@@ -1,9 +1,35 @@
 const mongoose = require('mongoose');
+const redis = require('redis');
+const util = require('util');
 
+const redisUrl = 'redis://127.0.0.1:6379';
+const client = redis.createClient(redisUrl);
+client.get = util.promisify(client.get);
 const exec = mongoose.Query.prototype.exec;
 
-mongoose.Query.prototype.exec = function() {
+mongoose.Query.prototype.exec = async function() {
   console.log('IM ABOUT TO RUN A QUERY');
 
-  return exec.apply(this, arguments);
+  console.log(this.getQuery());
+  console.log(this.mongooseCollection.name);
+
+  const key = JSON.stringify(
+    Object.assign({}, this.getQuery(), {
+      collection: this.mongooseCollection.name
+    })
+  );
+
+  // see if we have a value for 'key' in redis
+  const cacheValue = await client.get(key);
+
+  // If we do, return that
+  if (cacheValue) {
+    return JSON.parse(cacheValue);
+  }
+
+  // otherwise, issue the query and store the result in redis
+  const result = await exec.apply(this, arguments);
+  client.set(key, JSON.stringify(result));
+
+  return result;
 };
