d21fb54d0a6b98dbcf1d364251c7279dd4e159a4
zijld <d.zijlstra1989@gmail.com>
2019-07-03 06:38:37 +0200

    Add options key to cache chain & add middleware to clean cache

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
+const cleanCache = require('../middlewares/cleanCache');
 
 const Blog = mongoose.model('Blog');
 
@@ -15,7 +16,7 @@ module.exports = app => {
   });
 
   // POST: Create a blog post by user
-  app.post('/api/blogs', requireLogin, async (req, res) => {
+  app.post('/api/blogs', requireLogin, cleanCache, async (req, res) => {
     const { title, content } = req.body;
 
     const blog = new Blog({
@@ -34,7 +35,9 @@ module.exports = app => {
 
   // GET: All blogs by user.id
   app.get('/api/blogs', requireLogin, async (req, res) => {
-    const blogs = await Blog.find({ _user: req.user.id }).cache();
+    const blogs = await Blog.find({ _user: req.user.id }).cache({
+      key: req.user.id
+    });
 
     res.send(blogs);
   });
