97daa4a428b1d9c930f13dba0004e41728e0a861
zijld <d.zijlstra1989@gmail.com>
2019-07-04 08:12:51 +0200

    Add test: Launch a browser and check if the logo is there

--- tests/header.test.js
+++ tests/header.test.js
@@ -1,5 +1,20 @@
+const puppeteer = require('puppeteer');
+
 test('Adds two numbers', () => {
   const sum = 1 + 2;
 
   expect(sum).toEqual(3);
+});
+
+test('We can launch a browser', async () => {
+  const browser = await puppeteer.launch({
+    headless: false
+  });
+
+  const page = await browser.newPage();
+  await page.goto('localhost:3000');
+
+  const text = await page.$eval('a.brand-logo', el => el.innerHTML);
+
+  expect(text).toEqual('Blogster');
 });
