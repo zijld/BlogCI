dea324125b2f0522e7c7a5caf0a36e75cdac938b
zijld <d.zijlstra1989@gmail.com>
2019-07-08 10:51:12 +0200

    Cookie shit

--- tests/header.test.js
+++ tests/header.test.js
@@ -12,7 +12,7 @@ beforeEach(async () => {
 });
 
 afterEach(async () => {
-  await browser.close();
+  // await browser.close();
 });
 
 test('The header has the correct text', async () => {
@@ -26,4 +26,31 @@ test('clicking login starts oauth flow', async () => {
 
   const url = await page.url();
   expect(url).toMatch(/accounts\.google\.com/);
+});
+
+test.only('When signed in, shows logout button', async () => {
+  const id = '5d11221210fc043ef3c312ef';
+
+  const Buffer = require('safe-buffer').Buffer;
+  const sessionObject = {
+    passport: {
+      user: id
+    }
+  };
+  const sessionString = Buffer.from(JSON.stringify(sessionObject)).toString(
+    'base64'
+  );
+
+  const Keygrip = require('keygrip');
+  const keys = require('../config/keys');
+  const keygrip = new Keygrip([keys.cookieKey]);
+  const sig = keygrip.sign('session=' + sessionString);
+
+  const fakeCookie = [
+    { name: 'express:sess', value: sessionString },
+    { name: 'express:sess.sig', value: sig }
+  ];
+  await page.setCookie(...fakeCookie);
+
+  // await page.goto('localhost:3000');
 });
