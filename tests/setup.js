13168b1c5e70a56ac95cab16ee5c9f081880441f
zijld <d.zijlstra1989@gmail.com>
2019-07-10 06:44:43 +0200

    change deprecated configuration to recent ones

--- tests/setup.js
+++ tests/setup.js
@@ -5,6 +5,5 @@ const keys = require('../config/keys');
 
 mongoose.Promise = global.Promise;
 mongoose.connect(keys.mongoURI, {
-  useNewUrlParser: true,
-  useMongoClient: true
+  useNewUrlParser: true
 });
