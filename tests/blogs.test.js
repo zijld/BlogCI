a9ac3859e6ae1133e64fdada3c8f927d85ccfee3
zijld <d.zijlstra1989@gmail.com>
2019-07-18 20:55:08 +0200

    Create new test: User cannot create blog posts when not logged in

--- tests/blogs.test.js
+++ tests/blogs.test.js
@@ -62,3 +62,20 @@ describe('when logged in', () => {
     });
   });
 });
+
+describe('User is not logged in', () => {
+  test('User cannot create blog posts', async () => {
+    const result = await page.evaluate(() => {
+      return fetch('/api/blogs', {
+        method: 'POST',
+        credentials: 'same-origin',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ title: 'My Title', content: 'My Content' })
+      }).then(res => res.json());
+    });
+
+    expect(result).toEqual({ error: 'You must log in!' });
+  });
+});
