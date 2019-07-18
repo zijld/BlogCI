89feea0a175dd20baf27e9ea976e720bcaa2ae31
zijld <d.zijlstra1989@gmail.com>
2019-07-18 10:03:59 +0200

    create tests to create a blog using valid inputs

--- tests/blogs.test.js
+++ tests/blogs.test.js
@@ -23,6 +23,31 @@ describe('when logged in', () => {
     expect(label).toEqual('Blog Title');
   });
 
+  describe('And using valid inputs', () => {
+    beforeEach(async () => {
+      await page.type('.title input', 'My Title');
+      await page.type('.content input', 'My Content');
+      await page.click('form button[type="submit"]');
+    });
+
+    test('submitting takes user to review screen', async () => {
+      const text = await page.getContentsOf('h5');
+
+      expect(text).toEqual('Please confirm your entries');
+    });
+
+    test('submitting then saving adds blog to index page', async () => {
+      await page.click('button.green');
+      await page.waitFor('.card');
+
+      const title = await page.getContentsOf('.card-title');
+      const content = await page.getContentsOf('p');
+
+      expect(title).toEqual('My Title');
+      expect(content).toEqual('My Content');
+    });
+  });
+
   describe('And using invalid inputs', () => {
     beforeEach(async () => {
       await page.click('form button[type="submit"]');
