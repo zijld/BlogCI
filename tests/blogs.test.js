662543296f16a1d1b40e8a704dff69f9b3ecbcfd
zijld <d.zijlstra1989@gmail.com>
2019-07-19 13:54:43 +0200

    Create new test: User cannot get a list of posts when not logged in

--- tests/blogs.test.js
+++ tests/blogs.test.js
@@ -78,4 +78,18 @@ describe('User is not logged in', () => {
 
     expect(result).toEqual({ error: 'You must log in!' });
   });
+
+  test('User cannot get a list of posts', async () => {
+    const result = await page.evaluate(() => {
+      return fetch('/api/blogs', {
+        method: 'GET',
+        credential: 'same-origin',
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }).then(res => res.json());
+    });
+
+    expect(result).toEqual({ error: 'You must log in!' });
+  });
 });
