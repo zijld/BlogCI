e5835d72517605470ad30796e1e23dc820e9f0da
zijld <d.zijlstra1989@gmail.com>
2019-07-24 09:43:39 +0200

    test travis remove keys

--- config/ci.js
+++ config/ci.js
@@ -1,12 +1,3 @@
-// module.exports = {
-//   googleClientID:
-//     '244616794542-eb74she2775j30lggeujohra4u4pdnsf.apps.googleusercontent.com',
-//   googleClientSecret: 'zahfBpInbspRiWwRJw9dfmCz',
-//   mongoURI: 'mongodb://127.0.0.1:27017/blog_ci',
-//   cookieKey: 'greo4n3g83hu432toug',
-//   redisUrl: 'redis://127.0.0.1:6379'
-// };
-
 module.exports = {
   googleClientID: process.env.GOOGLE_CLIENT_ID,
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
--- config/dev.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  googleClientID:
-    '244616794542-eb74she2775j30lggeujohra4u4pdnsf.apps.googleusercontent.com',
-  googleClientSecret: 'zahfBpInbspRiWwRJw9dfmCz',
-  mongoURI:
-    'mongodb://bloguser:sevNNmfK9yYEd2@ds343217.mlab.com:43217/blog_app_dev',
-  cookieKey: 'asd',
-  redisUrl: 'redis://127.0.0.1:6379'
-};
