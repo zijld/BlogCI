24f2d37d7f9afb496c4db2be1bdef69afd9de1bd
zijld <d.zijlstra1989@gmail.com>
2019-07-24 09:22:42 +0200

    test travis build

--- config/ci.js
+++ config/ci.js
@@ -1,8 +1,16 @@
+// module.exports = {
+//   googleClientID:
+//     '244616794542-eb74she2775j30lggeujohra4u4pdnsf.apps.googleusercontent.com',
+//   googleClientSecret: 'zahfBpInbspRiWwRJw9dfmCz',
+//   mongoURI: 'mongodb://127.0.0.1:27017/blog_ci',
+//   cookieKey: 'greo4n3g83hu432toug',
+//   redisUrl: 'redis://127.0.0.1:6379'
+// };
+
 module.exports = {
-  googleClientID:
-    '244616794542-eb74she2775j30lggeujohra4u4pdnsf.apps.googleusercontent.com',
-  googleClientSecret: 'zahfBpInbspRiWwRJw9dfmCz',
-  mongoURI: 'mongodb://127.0.0.1:27017/blog_ci',
-  cookieKey: 'askgnewjo17898933b6hk3lwl56eajfn',
-  redisUrl: 'redis://127.0.0.1:6379'
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  mongoURI: process.env.MONGO_URI,
+  cookieKey: process.env.COOKIE_KEY,
+  redisUrl: process.env.REDIS_URL
 };
--- config/dev.js
+++ config/dev.js
@@ -4,6 +4,6 @@ module.exports = {
   googleClientSecret: 'zahfBpInbspRiWwRJw9dfmCz',
   mongoURI:
     'mongodb://bloguser:sevNNmfK9yYEd2@ds343217.mlab.com:43217/blog_app_dev',
-  cookieKey: 'askgnewjo17898933b6hk3lwl56eajfn',
+  cookieKey: 'asd',
   redisUrl: 'redis://127.0.0.1:6379'
 };
